56fd062f1dd25bca5ca854e549af2d6b01b5fcde
bbffbb <birkirfb13@ru.is>
2016-11-08 02:15:40 +0000

    meh

---
+++ public/js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('ul.tabs').tabs();
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
