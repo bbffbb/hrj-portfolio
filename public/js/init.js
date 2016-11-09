b593d8cea62a17cf6c74e15626c946c289e66623
bbffbb <birkirfb13@ru.is>
2016-11-09 20:42:54 +0000

    v1

--- public/js/init.js
+++ public/js/init.js
@@ -1,10 +1,20 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav({
+      menuWidth: 100,
+      closeOnClick: true,
+      draggable: true
+    });
     $('.parallax').parallax();
-    $('.slider').slider();
+
     $('ul.tabs').tabs();
+    $('.modal').modal();
+    $('.slider').slider({
+      full_width: true,
+      height: 545
+    });
+
 
 
   }); // end of document ready
